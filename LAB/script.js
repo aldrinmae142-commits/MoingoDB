c0a06dbb8cb26b183adea31891b465d212fab1f8
aldrinmae142-commits <aldrinmae142@gmail.com>
2026-04-19 18:24:50 +0800

    Add files via upload

---
+++ LAB/script.js
@@ -0,0 +1,20 @@
+// Conditional statements
+let num = 10;
+if (num > 0) {
+    console.log("Positive");
+} else if (num < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
+
+// Loops
+for (let i = 0; i < 5; i++) {
+    console.log("Loop " + i);
+}
+
+// Functions
+function greet(name) {
+    return "Hello " + name;
+}
+console.log(greet("World"));
